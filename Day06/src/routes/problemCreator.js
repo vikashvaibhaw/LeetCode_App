87ac9dc4d74e5f0a692d768d2eb3d0a8a90115d7
vikashvaibhaw <vvaibhaw6@gmail.com>
2025-12-28 23:05:46 +0530

    Add Day 06 work

---
+++ Day06/src/routes/problemCreator.js
@@ -0,0 +1,15 @@
+const express=require('express')
+const problemRouter=express.Router();
+const adminMiddleware = require('../Middleware/adminMiddleware');
+const {createProblem,updateProblem,deleteProblem,getProblemById,getAllProblem,solvedAllProblemByUser}=require("../controllers/userProblem");
+const userMiddleware = require('../Middleware/userMiddleware');
+
+problemRouter.post("/create",adminMiddleware,createProblem);
+problemRouter.put("/update/:id",adminMiddleware,updateProblem);
+problemRouter.delete("/delete/:id",adminMiddleware,deleteProblem); 
+
+problemRouter.get("/problemById/:id",userMiddleware,getProblemById);
+problemRouter.get("/getAllProblem",userMiddleware,getAllProblem); 
+problemRouter.get("/problemSolvedByUser",userMiddleware,solvedAllProblemByUser);
+
+module.exports=problemRouter
